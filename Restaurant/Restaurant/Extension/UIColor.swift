ff767cf039036fc46b1c5f2486e72abb820351c2
nxan <nxan.developer@gmail.com>
2019-06-15 14:22:38 +0700

    commit

---
+++ Restaurant/Restaurant/Extension/UIColor.swift
@@ -0,0 +1,78 @@
+//
+//  UIColor.swift
+//  ShareAds
+//
+//  Created by NXA on 3/19/19.
+//  Copyright © 2019 NXA. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return String(format:"#%06x", rgb)
+    }
+    
+    static var vegaRed: UIColor {
+        return UIColor(red:0.90, green:0.22, blue:0.21, alpha:1.0)
+    }
+    
+    static var vegaGreen: UIColor {
+        return UIColor(red:0.30, green:0.69, blue:0.31, alpha:1.0)
+    }
+    
+    static var vegaGray: UIColor {
+        return UIColor(red:0.51, green:0.51, blue:0.55, alpha:1.0)
+    }
+    
+    static var vegaBlack: UIColor {
+        return UIColor(red:0.12, green:0.12, blue:0.12, alpha:1.0)
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
